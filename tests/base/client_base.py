2cc4ff03f0ee05667fbf17b60a142526dbbb2fe9
zhuwenxing <wenxing.zhu@zilliz.com>
2023-04-12 14:29:00 +0800

    test: add max tasks num testcase (#10)
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/base/client_base.py
+++ tests/base/client_base.py
@@ -1,7 +1,20 @@
 from pymilvus import connections
+from api.milvus_cdc import MilvusCdcClient
+
+client = MilvusCdcClient('http://localhost:8444')
 
 
 class TestBase:
+
+    def setup_method(self, method):
+        res, result = client.list_tasks()
+        # delete the tasks
+        for task in res["tasks"]:
+            task_id = task["task_id"]
+            rsp, result = client.delete_task(task_id)
+            assert result
+            assert rsp == {}
+
     def teardown_method(self, method):
         if len(connections.list_connections()) > 0:
             for conn in connections.list_connections():
