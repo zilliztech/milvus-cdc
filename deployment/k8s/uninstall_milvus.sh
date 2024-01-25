6529521639b8ae009ebf476cb4e7ba59eb93c3d3
zhuwenxing <wenxing.zhu@zilliz.com>
2024-01-25 19:01:48 +0800

    ci: clean unused file
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- deployment/k8s/uninstall_milvus.sh
+++
@@ -1,8 +0,0 @@
-
-# Exit immediately for non zero status
-set -e
-release=${1:-"milvus-chaos"}
-ns=${2:-"chaos-testing"}
-helm uninstall ${release} -n=${ns}
-kubectl delete pvc -l release=${release} -n=${ns}
-kubectl delete pvc -l app.kubernetes.io/instance=${release} -n=${ns}
